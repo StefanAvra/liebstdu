92f905e3599d16a6f8da945baccf13670e6e4468
stefanavra <stefan.avra@gmail.com>
2020-05-27 23:39:35 +0200

    changed thing to name in Thing model

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class Thing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    thing = db.Column(db.String(120), index=True, unique=True)
+    name = db.Column(db.String(120), index=True, unique=True)
 
     def __repr__(self):
-        return '<Thing {}>'.format(self.thing)
+        return '<Thing {}>'.format(self.name)
