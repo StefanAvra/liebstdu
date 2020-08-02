81b7abc1d275390904e5c4674b8331db6bcde8ac
stefanavra <stefan.avra@gmail.com>
2020-08-02 19:13:31 +0200

    changed routes to get things from db

--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,35 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from random import randint
+from app.models import User, Thing, Vote
+from  sqlalchemy.sql.expression import func, select
 
-things = [
-    ('Fun', 0),
-    ('Wurst', 0),
-    ('Pizza', 0),
-    ('Abli', 0),
-    ('Sex', 0),
-    ('Babes', 1),
-    ('Maultaschen', 1),
-    ('schlafen', 0),
-    ('Essen', 0),
-    ('Dreads', 1),
-    ('Sonne', 0),
-    ('Japan', 0),
-    ('Bolognese', 0),
-    ('Tee', 0),
-    ('Kaffee', 0),
-    ('Spiele', 1),
-    ('Bärlauch', 0),
-    ('Pflanzen', 1)
-]
 
 @app.route('/')
-@app.route('/<thing>')
-def index(thing=None, plural=0):
-    if not thing:
-        selector = randint(0, len(things)-1)
-        thing = things[selector][0]
-        plural = things[selector][1]
-        thing = thing.capitalize()
-    return render_template('index.html', thing=thing, plural=plural)
+@app.route('/<p_thing>')
+def index(p_thing=None, plural=0):
+    # if not thing:
+    #     selector = randint(0, len(things)-1)
+    #     thing = things[selector][0]
+    #     plural = things[selector][1]
+    #     thing = thing.capitalize()
+    # return render_template('index.html', thing=thing, plural=plural)
+    if not p_thing:
+        rowCount = Thing.query.count()
+        thing = Thing.query.offset(randint(0, rowCount-1)).first()
+    else:
+        thing = Thing.query.filter(Thing.name.ilike(p_thing)).first()
+    try:
+        t = {'id': thing.id, 'name': thing.name, 'plural': thing.plural}
+    except:
+        t = 'no thing {} found'.format(p_thing)
+        
+    return t
+
+@app.route('/things')
+def get_things():
+    all_things = {}
+    for thing in Thing.query.all():
+        all_things[thing.id] = {'name': thing.name, 'plural': thing.plural}
+    return all_things
+
