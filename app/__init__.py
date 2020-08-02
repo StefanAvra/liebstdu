a1ca21ec6cad1f4c58dbe4f37563da5b9e84492f
stefanavra <stefan.avra@gmail.com>
2020-08-03 00:10:56 +0200

    added instance config

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(Config)
+app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
